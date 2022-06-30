f2ad5df3e4a4a54cfc2747bb3938834f18182e28
Sean Muzungu <seanmuzungu.com>
2022-06-30 09:05:50 -0700

    puts2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <string.h>
 
-int main() {
-	char str1[] = "\"Programming is like building a multilingual puzzle";
+int main(void) {
+	str1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 
