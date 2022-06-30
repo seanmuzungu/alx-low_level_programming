0a740b2a5ef06fc0926b5e3d76d0cf2aaa1de0d3
Sean Muzungu <seanmuzungu.com>
2022-06-30 09:27:18 -0700

    Outs fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
+int main(void)
+{
 	char str1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 
-	return 0;
+	return (0);
 }
+
