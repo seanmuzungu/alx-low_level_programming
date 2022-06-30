f3a0b9c06087fde691173cb18b00dab9e0ff6dfb
Sean Muzungu <seanmuzungu.com>
2022-06-30 10:55:29 -0700

    line

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+/* 
+ * main - Entry point *
+ *
+ * *  Description: 'the program's description'
+ *
+ * *  Return: Always 0 (Success) 
+ */
+
+int main(void)
+i{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+	
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu byte\n", sizeof(charType));
+
+	return (0);
+}
+
