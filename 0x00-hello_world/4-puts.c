d99b2e1637f62d833f9a5380d8ced5f9dbd0fae4
Sean Muzungu <seanmuzungu.com>
2022-06-30 09:46:56 -0700

    Fix Betty linters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ *  * main - Entry point
+ *  Description: 'the program's description'
+ *  Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
@@ -9,4 +15,3 @@ int main(void)
 
 	return (0);
 }
-
