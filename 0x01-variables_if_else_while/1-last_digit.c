665a47225ba3b29df88bfe87806308d593572752
Sean Muzungu <seanmuzungu.com>
2022-07-04 13:36:27 -0700

    Fix6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ else if (n == 0)
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0\n, n. lastn");
+	printf("Last digit of %d is %d and is less than 6 and not 0\n, n, lastn");
 }
 
 	return (0);
