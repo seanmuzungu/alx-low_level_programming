3b548c22bc9cfa0ed9e0c84314c3bc6898be7cc7
Sean Muzungu <seanmuzungu.com>
2022-07-04 15:00:24 -0700

    Al[habet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main entry point
+ *
+ * Return: Always retuns 0
+ */
+
+int main(void)
+{
+	char a;
+	for ( a = 'a'; a <= 'z'; a++)
+	putchar(a);
+	putchar('\n');
+
+	return (0);
+}
