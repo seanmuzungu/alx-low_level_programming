adff5a8e2bb4689cee46a7f536927ef7b90b3ee3
Sean Muzungu <seanmuzungu.com>
2022-07-04 16:30:44 -0700

    Char

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * * * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+{
+	if (a = 'q' || a = 'e')
+{
+	continue;
+	outchar(a);
+
+	return (0);
+}
