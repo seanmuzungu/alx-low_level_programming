1256b1e37cab3c458cf0836145212ffe588ba990
Sean Muzungu <seanmuzungu.com>
2022-07-04 18:48:41 -0700

    lkilo

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/** 
+ *  * * main - Entry point
+ *  Description: 'the program's description' 
+ *  Return: Always 0 (Success): 
+ */ 
+int main(void) 
+{
+	unsigned char = '0';
+	int i;
+	for(i = 0; i < 10; i++)
+{
+	putchar(a);
+	a++;
+}
+	a = '1';
+	for(i = 0; i < 10; i++)
+	putchar('0' + a);
+{
+	a++;
+}
+	putchar('\n');
+	return (0);
+}
