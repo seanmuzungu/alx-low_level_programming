86f46534063928abf4d8c031597472e712e29983
Sean Muzungu <seanmuzungu.com>
2022-07-04 17:39:37 -0700

    pp

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** 
+ * * * main - Entry point 
+ * Description: 'the program's description' 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (;a < 10; a++)
+	printf(%d, a);
+	putchar('\n');
+	return (0);
+}
