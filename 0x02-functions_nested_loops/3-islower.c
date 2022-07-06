12c449f998f140777d3dad9f07bfee426f09e47d
Sean Muzungu <seanmuzungu.com>
2022-07-06 12:40:38 -0700

    check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+# include "main.h"
+
+/**
+ * _islower - checks for lowercase 'c'
+ * @c: character argumet
+ * Retun: 0 or 1 depending
+ */
+
+int _islower(int c)
+{
+	return (c >= 'c' <= 'z');
+}
