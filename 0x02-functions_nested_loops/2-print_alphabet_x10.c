9cd79d59e0af67aed9ceec7601a09aeccdafab9f
Sean Muzungu <seanmuzungu.com>
2022-07-06 12:27:08 -0700

    Fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
+ * print_alphabet_x10 - prints alphanets 10 times
  *
+ *  Return: 0
  */
 
 void print_alphabet_x10(void)
@@ -9,12 +11,12 @@ void print_alphabet_x10(void)
 	int j;
 	int x;
 
-	for ( x = 0; x <= 10; x++)
+	for (x = 0; x <= 10; x++)
 	{
-		for (j = 'a'; j <= 'z'; j++) 
+		for (j = 'a'; j < 'z'; j++)
 		{
-		_putchar(j); 
+			_putchar(j);
 		}
-       		_putchar('\n');
+		_putchar('\n');
 	}
 }
