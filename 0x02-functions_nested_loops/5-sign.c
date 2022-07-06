2481cf00413ea31f33f06bf122c2ec849fbf184c
Sean Muzungu <seanmuzungu.com>
2022-07-06 13:31:17 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
+/**
+ * print_sign - print sign accordingly
+ * @n: character argument passed
+ * Return: 1 0 -1 depending
+ */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('=');
-		return(1);
+		_putchar ('=');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
-		return(0);
+		_putchar ('0');
+		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
-		return(-1);
+		_putchar ('-');
+		return (-1);
 	}
 }
