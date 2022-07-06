187dfc7fb335bfc5c0f18e4bf706faba698f80ce
Sean Muzungu <seanmuzungu.com>
2022-07-06 11:39:59 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+
+void print_alphabet(void);
+{
+	int j:
+	for (j = 'a'; j <= 'z' j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\');
+
+	return (0);
+}
