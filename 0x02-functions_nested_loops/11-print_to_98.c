ac972530ab9efac2bd0808800e6ce81f699a1d24
Sean Muzungu <seanmuzungu.com>
2022-07-06 16:32:35 -0700

    add

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,14 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			_putchar("%d, ", n--);
-		_putchar("%d\n", n);
+			_putchar('%d, ', n--);
+		_putchar('%d\n', n);
 	}
 	else
 	{
 		while (n < 98)
-			_putchar("%d, ", n++);
-		_putchar("%d\n", n);
+			_putchar('%d, ', n++);
+		_putchar('%d\n', n);
 	}
 }
 
