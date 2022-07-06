435a900c2c7039a26c53738fd95fd759a3848763
Sean Muzungu <seanmuzungu.com>
2022-07-06 15:00:57 -0700

    digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#includes "mai.h"
+
+/**
+ * print_last_digit - prunts last digit
+ * @n: parameter
+ * Return: returns 1
+ */
+
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n > 0)
+		n = -n;
+	x = n % 10;
+	_putchar (x + '0');
+	return (n % 10);
+
+}
