0dc71a0d287847f932ce5110521f341e0a0490e4
Sean Muzungu <seanmuzungu.com>
2022-07-06 15:29:00 -0700

    digit1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,9 @@
 
 int print_last_digit(int n)
 {
-	int x;
-
-	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar (x + '0');
-	return (x);
-
+		int last_digit = n % 10;
+		if (last_digit < 0)
+		last_digit *= -1;
+		_putchar(last_digit + '0');
+		return (last_digit);
 }
