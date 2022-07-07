c3d5329f73d049a02d3bc17655163309466ebb6a
Sean Muzungu <seanmuzungu.com>
2022-07-07 09:15:52 -0700

    Is an Upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: The parameter
+ * Return: 1 for uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+#0x04-more_functions_nested_loops
+
+This is a project on advanced functions and nested loops
+
