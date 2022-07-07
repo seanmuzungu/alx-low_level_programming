bb65369524d15d5272275f47157683d8c98dc0a0
Sean Muzungu <seanmuzungu.com>
2022-07-06 17:41:54 -0700

    Fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - all the numbers below 1024
- *  Return: Always 0. 
+ * Return: Always 0.
  */
 
 int main(void)
@@ -11,10 +11,9 @@ int main(void)
 	
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0) 
+		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
 	printf("%d\n", sum);
-	
 	return (0);
 }
