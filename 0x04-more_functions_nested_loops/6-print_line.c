9d47616d7d96831a24b46c7d9a663f13962636c3
Sean Muzungu <seanmuzungu.com>
2022-07-07 14:00:38 -0700

    draw

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal.
+ * @n: Thei parama
+ * Returns: 0
+ */
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
