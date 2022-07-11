9f2f69930bd3985f23218ed05f0515d6288db955
Sean Muzungu <seanmuzungu.com>
2022-07-11 07:13:55 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+
+void print_rev(char *s)
+{
+	int length = 0, index;
+
+	while (s[index++])
+		legnth++;
+	for (index = length - 1; index >= 0; index--)
+		_putchar(s[index]);
+	_putchar('\n');
+}
+
