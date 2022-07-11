f07e4c23e6b80444c67696cf183d23aa67bb3baf
Sean Muzungu <seanmuzungu.com>
2022-07-11 07:15:19 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 	int length = 0, index;
 
 	while (s[index++])
-		legnth++;
+		length++;
 	for (index = length - 1; index >= 0; index--)
 		_putchar(s[index]);
 	_putchar('\n');
