9ed1aff8513a77d4c277d1d46d6826d1ea62ad3c
Sean Muzungu <seanmuzungu.com>
2022-07-11 06:59:45 -0700

    Memoery pointer

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 
-/** 
+/**
  * swap_int - Swaps the values of two integers.
  * @a: The first interger.
  * @b: The second interger.
  */
- void swap_int(int *a, int *b)
+
+void swap_int(int *a, int *b)
 {
-		int tmp = *a;
-			*a = *b;
-				*b = tmp;
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
 }
