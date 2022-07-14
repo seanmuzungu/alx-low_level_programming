db2a50b30d73d071b74c400f2cc77351fc74e133
Sean Muzungu <seanmuzungu.com>
2022-07-14 02:21:47 -0700

    Fix0 again

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,14 @@
 
 char *_strcat(char *dest, char *src)
 {
- int a = -1, i;
+	int a = -1, i;
 
- for (i = 0; dest[i] != '\0'; i++)
- ;
-
- do {
-  a++;
-  dest[i] = src[a];
-  i++;
- } while (src[a] != '\0');
-
- return (dest);
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+	return (dest);
 }
