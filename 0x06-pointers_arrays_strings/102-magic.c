f2b580b24e270eeee81b6483317de2abef68d929
Sean Muzungu <seanmuzungu.com>
2022-07-14 08:25:14 -0700

    Magic fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,46 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
- *
- * Return: A pointer to the encoded string.
- */
-char *rot13(char *str)
+int main(void)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-			     'G', 'H', 'I', 'J', 'K', 'L',
-			     'M', 'N', 'O', 'P', 'Q', 'R',
-			     'S', 'T', 'U', 'V', 'W', 'X',
-			     'Y', 'Z', 'a', 'b', 'c', 'd',
-			     'e', 'f', 'g', 'h', 'i', 'j',
-			     'k', 'l', 'm', 'n', 'o', 'p',
-			     'q', 'r', 's', 't', 'u', 'v',
-			     'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-			     'T', 'U', 'V', 'W', 'X', 'Y',
-			     'Z', 'A', 'B', 'C', 'D', 'E',
-			     'F', 'G', 'H', 'I', 'J', 'K',
-			     'L', 'M', 'n', 'o', 'p', 'q',
-			     'r', 's', 't', 'u', 'v', 'w',
-			     'x', 'y', 'z', 'a', 'b', 'c',
-			     'd', 'e', 'f', 'g', 'h', 'i',
-			     'j', 'k', 'l', 'm'};
+	  int n;
+	    int a[5];
+	      int *p;
 
-	while (str[indx1])
-	{
-		for (indx2 = 0; indx2 < 52; indx2++)
-		{
-			if (str[indx1] == alphabet[indx2])
-			{
-				str[indx1] = rot13key[indx2];
-				break;
-			}
-		}
-
-		indx1++;
-
-	}
-	return (str);
+	        a[2] = 1024;
+		  p = &n;
+		    /*
+		     *    * write your line of code here...
+		     *       * Remember:
+		     *          * - you are not allowed to use a
+		     *             * - you are not allowed to modify p
+		     *                * - only one statement
+		     *                   * - you are not allowed to code anything else than this line of code
+		     *                      */
+		   p[5] = 98;
+		     /* ...so that this prints 98\n */
+		     printf("a[2] = %d\n", a[2]);
+		       return (0);
 }
