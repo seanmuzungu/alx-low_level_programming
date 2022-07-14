337c9c1f9601e38c122255c961218ee35a11347d
Sean Muzungu <seanmuzungu.com>
2022-07-14 08:23:24 -0700

    Fix11 2 times

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- *  * @n: input integer.
+ * print_number - printsthe integer
+ * @n: input integer.
  *   * Return: no return.
- *    */
- void print_number(int n)
+ */
+
+void print_number(int n)
 {
 	unsigned int m, d, count;
 
