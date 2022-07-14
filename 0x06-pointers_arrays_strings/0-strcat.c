52414288948eed27d7923b6d4d8916538124b4e7
Sean Muzungu <seanmuzungu.com>
2022-07-14 01:20:58 -0700

    Fix0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,15 @@
  */
 
 char *_strcat(char *dest, char *src)
-{
-	int index = 0;
+i{
+	int index;
 	int dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] != '\0'; index++){
 		dest[dest_len++] = src[index];
+	dest_len +=1;
+	}
 	return (dest);
 }
