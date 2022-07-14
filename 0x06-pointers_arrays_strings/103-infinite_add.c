81dbdc9fb5f10d163bba201083f06e069a2321ae
Sean Muzungu <seanmuzungu.com>
2022-07-14 08:30:13 -0700

    Fix add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,32 +1,27 @@
 #include "main.h"
 
 /**
- * infinite_add - Adds two numbers.
- * @n1: The first number to be added.
- * @n2: The second number to be added.
- * @r: The buffer to store the result.
- * @size_r: The buffer size.
- *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
- */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+ *  * rot13 - encodes a string in rot13
+ *   * @s: string to be encoded
+ *    * Return: the resulting strring
+ *     */
+char *rot13(char *s)
 {
-	int index, n1_len = 0, n2_len = 0;
+		int i, j;
 
-	for (index = 0; *(n1 + index); index++)
-		n1_len++;
+			char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+				char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (index = 0; *(n2 + index); index++)
-		n2_len++;
-
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-		return (0);
-
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
-	*(r + size_r) = '\0';
-
-	return (add_strings(n1, n2, r, --size_r));
+					for (i = 0; s[i] != '\0'; i++)
+							{
+										for (j = 0; a[j] != '\0'; j++)
+													{
+																	if (s[i] == a[j])
+																					{
+																										s[i] = b[j];
+																														break;
+																																	}
+																		    }
+											}
+						return (s);
 }
