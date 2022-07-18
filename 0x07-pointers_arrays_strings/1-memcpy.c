d83a05588eb674a6a5d7450bd1f51a5961195a44
Sean Muzungu <seanmuzungu.com>
2022-07-18 13:21:43 -0700

    Add files

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies to memeory
+ * @dest: A pointer to the memory area
+ * @src: The source buffer to copy
+ * @n: The number of bytes to copy
+ * Return: A pointer 
+ */
+
+void *_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned int index;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	for (index = 0; index < n; index++)
+		destination[index] = source[index];
+
+	return (dest);
+}
