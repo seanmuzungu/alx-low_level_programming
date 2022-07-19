4940dc38ac384041dd6e72fdcb40290473a41660
Sean Muzungu <seanmuzungu.com>
2022-07-19 06:01:54 -0700

    gggg

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * memset - simple_print_buffer - prints buffer in hexa
+ * memset - simple_print_buffer
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
- *
  * Return: A pointer to the filled memory area @s.
  */
 
