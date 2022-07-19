61dfb928758095376c49190caeae72240b8fd528
Sean Muzungu <seanmuzungu.com>
2022-07-19 11:39:25 -0700

    Fix100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,14 +1,5 @@
 #include "main.h"
 
-
-/*
- *:wq
-File: 9-set_string.c
- * Auth: Brennan D Baraban
- */
-
-#include "main.h"
-
 /**
  * set_string - Sets the value of a pointer to a char.
  * @s: The pointer.
