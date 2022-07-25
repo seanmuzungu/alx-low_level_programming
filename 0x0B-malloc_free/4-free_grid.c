8faf0d24e854be21a304f2efe40f6e520b4fbf86
Sean Muzungu <seanmuzungu.com>
2022-07-24 23:45:31 -0700

    NAdd all

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+/**
+ *free_grid-frees a 2d array of integer
+ *@grid:The 2D array of integer to be freed
+ *@height: The height of grid
+ */
+
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
+Footer
+
