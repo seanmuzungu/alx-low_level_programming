696524eb36c9297a1333e395f9172edd6cb8e946
Sean Muzungu <seanmuzungu.com>
2022-07-28 05:51:28 -0700

    AddAll

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *string_nconcat-concatinates two strings
+ *@s1: holberton
+ *@s2: school
+ *@n:checked
+ *Return: concatinated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *varcon;
+
+	unsigned int i, j, len = n;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i]; i++)
+		;
+
+	for (j = 0; s2[j]; j++)
+		;
+
+	if (j > n)
+		j = n;
+
+	varcon = malloc(sizeof(char) * (i + j + 1));
+
+	if (varcon == NULL)
+		return (NULL);
+
+	for (len = 0; len < i; len++)
+	{
+		varcon[len] = s1[len];
+	}
+
+	for (len = 0; len < j; len++)
+	{
+		varcon[len + i] = s2[len];
+	}
+
+	varcon[i + j] = '\0';
+
+	return (varcon);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#nclude "main.h"
+include <stdlib.h>
+
+/**
+ *array_range- cretes array of integer
+ *@max: last value of the array
+ *@min: first value of the array
+ *Return: NULL if min > max and if array equall to NULL
+ *        otherwise an array
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, i, size;
+
+	if (min > max)
+		return (NULL);
+	size = max - min;
+
+	array = malloc(sizeof(int) * (size + 1));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = i + min;
+	array[size] = max;
+	return (array);
+}
