6c7249240c644603c07e851d72a1462543e33cc5
Sean Muzungu <seanmuzungu.com>
2022-08-02 05:20:19 -0700

    add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,4 @@
-i#include "dog.h"
+#include "dog.h"
 #include <stdlib.h>
 
 int _strlen(char *str);
@@ -8,14 +8,15 @@ dog_t *new_dog(char *name, float age, char *owner);
  *  *_strlen-finds length of string
  *   *@str: the string to be mmeasured
  *    *Return: the length of the string
- */
+ *     */
 
 int _strlen(char *str)
 {
-	int len = 0;
-	while (str[len] != '\0')
-		len++;
-	return (len);
+		int len = 0;
+
+			while (str[len] != '\0')
+						len++;
+				return (len);
 }
 
 /**
@@ -24,43 +25,53 @@ int _strlen(char *str)
  *    *@age:age of the dog
  *     *@owner: owner of the dog
  *      *Return: Shappered
- */
-
+ *       */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *shappered;
-	int nameLength, ownerLength, i;
-	nameLength = _strlen(name);
-	ownerLength = _strlen(owner);
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-	shappered = malloc(sizeof(dog_t));
-	if (shappered == NULL)
-		return (NULL);
-	nameLength = _strlen(name) + 1;
-	shappered->name = malloc(sizeof(char) * nameLength);
-	if (shappered->name == NULL)
-	{
-		free(shappered->name);
-		free(shappered);
-		return (NULL);
-	}
-	for (i = 0; i < nameLength; i++)
-	{
-		shappered->name[i] = name[i];
-	}
-	shappered->age = age;
-	ownerLength = _strlen(owner) + 1;
-	shappered->owner =  malloc(sizeof(char) * ownerLength);
-	if (shappered->owner == NULL)
-	{
-		free(shappered->name);
-		free(shappered->owner);
-		free(shappered);
-		return (NULL);
-	}
-	for (i = 0; i < ownerLength; i++){
-		shappered->owner[i] = owner[i];
-	}
-	return (shappered);
-}
+		dog_t *shappered;
+			int nameLength, ownerLength, i;
+
+				nameLength = _strlen(name);
+					ownerLength = _strlen(owner);
+
+							if (name == NULL || age < 0 || owner == NULL)
+											return (NULL);
+
+								shappered = malloc(sizeof(dog_t));
+									if (shappered == NULL)
+
+												return (NULL);
+
+										nameLength = _strlen(name) + 1;
+											shappered->name = malloc(sizeof(char) * nameLength);
+												if (shappered->name == NULL)
+														{
+																	free(shappered->name);
+																			free(shappered);
+																					return (NULL);
+																						}
+
+													for (i = 0; i < nameLength; i++)
+															{
+																		shappered->name[i] = name[i];
+																			}
+
+														shappered->age = age;
+
+
+															ownerLength = _strlen(owner) + 1;
+																shappered->owner =  malloc(sizeof(char) * ownerLength);
+																	if (shappered->owner == NULL)
+																			{
+																						free(shappered->name);
+																								free(shappered->owner);
+																										free(shappered);
+																												return (NULL);
+																													}
+
+																		for (i = 0; i < ownerLength; i++)
+																				{
+																							shappered->owner[i] = owner[i];
+																								}
+
+																			return (shapper
