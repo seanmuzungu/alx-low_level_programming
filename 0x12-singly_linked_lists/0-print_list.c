484b50342606ca743c9e8fe1e6653e9f2bc2be51
Sean Muzungu <seanmuzungu.com>
2022-08-11 12:43:32 -0700

    Fix last

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,29 +1,23 @@
-#include <stdlib.h>
-
+#include "lists.h"
 /**
- *create_array- creats array
- *@c: checked
- *@size: size of the array
- *Return: array
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list
  */
 
-char *create_array(unsigned int size, char c)
+size_t print_list(const list_t *h)
 {
-	char *array;
+ size_t nelem;
 
-	unsigned int i;
-
-	if (size == 0)
-		return (NULL);
-
-	array = malloc(sizeof(char) * size);
-
-	if (array == NULL)
-
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		array[i] = c;
-
-	return (array);
+ nelem = 0;
+ while (h != NULL)
+ {
+  if (h->str == NULL)
+   printf("[%d] %s\n", 0, "(nil)");
+  else
+   printf("[%d] %s\n", h->len, h->str);
+  h = h->next;
+  nelem++;
+ }
+ return (nelem);
 }
