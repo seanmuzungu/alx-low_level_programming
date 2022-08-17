2396f30a3409068eda55b124c6267b03f7067bd2
Sean Muzungu <seanmuzungu.com>
2022-08-17 01:12:29 -0700

    Fix 5 & 6

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,19 +2,19 @@
 /**
  *  *free_listint2- free's listint_t and sets the head to NULL
  *   *@head: a ptr tp the listint_t list
- *    */
+ */
+
 void free_listint2(listint_t **head)
 {
-		listint_t *temp;
+	listint_t *temp;
 
-			if (head == NULL)
-						return;
-
-				while (*head != NULL)
-						{
-									temp = (*head)->next;
-											free(*head);
-													*head = temp;
-														}
-					*head = NULL;
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
 }
