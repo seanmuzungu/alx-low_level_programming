4d0415d1e4a661a6c05ffae58e0d49f23efbe9d6
Sean Muzungu <seanmuzungu.com>
2022-08-17 01:20:57 -0700

    Fix 7 & 8

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,18 +4,19 @@
  *   *@head: a ptr the listint_t list
  *    *@index: is the index of the node, starting at 0
  *     *Return: the nth node of a listint_t linked list.
- *      */
+ */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-		unsigned int count = 0;
+	unsigned int count = 0;
 
+	while (count < index)
+	{
+		if (head == NULL)
+			return (NULL);
 
-			while (count < index)
-					{
-								if (head == NULL)
-												return (NULL);
-										count++;
-												head  = head->next;
-													}
-				return (head);
+		count++;
+		head  = head->next;
+	}
+	return (head);
 }
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,15 +3,16 @@
  *  *sum_listint- gives the sum of of all the data.
  *   *@head: a ptr the listint_t list
  *    *Return: the sum of the data if it is empty 0.
- *     */
+ */
+
 int sum_listint(listint_t *head)
 {
-		int sum = 0;
+	int sum = 0;
 
-			while (head)
-					{
-								sum += head->n;
-										head = head->next;
-											}
-				return (sum);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
