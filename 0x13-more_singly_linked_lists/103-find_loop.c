0e6d662c48ac72551f80f5e849dfe7c1e811fe00
Sean Muzungu <seanmuzungu.com>
2022-08-17 01:45:45 -0700

    Fix 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,35 +7,33 @@
  *     *
  *      * Return: If there is no loop - NULL.
  *       *         Otherwise - the address of the node where the loop starts.
- *        */
+ */
+
 listint_t *find_listint_loop(listint_t *head)
 {
-		listint_t *tortoise, *hare;
+	listint_t *tortoise, *hare;
 
-			if (head == NULL || head->next == NULL)
-						return (NULL);
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+	tortoise = head->next;
+	hare = (head->next)->next;
 
-				tortoise = head->next;
-					hare = (head->next)->next;
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
 
-						while (hare)
-								{
-											if (tortoise == hare)
-														{
-																		tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
 
-																					while (tortoise != hare)
-																									{
-																														tortoise = tortoise->next;
-																																		hare = hare->next;
-																																					}
-
-																								return (tortoise);
-																										}
-
-													tortoise = tortoise->next;
-															hare = (hare->next)->next;
-																}
-
-							return (NULL);
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (NULL);
 }
