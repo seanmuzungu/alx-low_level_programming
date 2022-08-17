9f5e55c2c6e5973c9794cc34d87f096693db270d
Sean Muzungu <seanmuzungu.com>
2022-08-17 00:55:54 -0700

    Fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,34 +1,18 @@
 #include "lists.h"
 
 /**
- *  *add_nodeint_end - adds a new node at the end  of a list_t list
- *   *@head: a ptr to the head of listint_t list
- *    *@n: the number to be added to the listint_t list
- *     *
- *      *Return: the address of the new element, or NULL if it failed
- *       */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+ *  * free_listint - Frees a listint_t list.
+ *   * @head: A pointer to the head of the listint_t list to be freed.
+ */
+
+void free_listint(listint_t *head)
 {
+	listint_t *tmp;
 
-		listint_t *nptr, *temp;
-
-			nptr  = malloc(sizeof(listint_t));
-				if (nptr == NULL)
-							return (NULL);
-
-
-
-					nptr->n = n;
-						nptr->next = NULL;
-
-							if (*head == NULL)
-										*head = nptr;
-								else
-										{
-													temp = *head;
-															while (temp->next != NULL)
-																			temp = temp->next;
-																	temp->next = nptr;
-																		}
-									return (*head);
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
 }
