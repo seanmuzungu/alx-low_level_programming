bddf6e93c6a7af2b180e092e25df1d8fda4ebfa5
Sean Muzungu <seanmuzungu.com>
2022-08-18 10:54:41 -0700

    Add 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,7 @@
  *   * AUth: Brennan D Baraban
  *    * Desc: Header file containing prototypes for all functions
  *     *       written in the 0x13-bit_manipulation directory.
- *      */
+ */
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
